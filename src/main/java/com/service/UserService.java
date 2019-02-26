d31eec7925b5e3e2b7ce8b6c13d11fc4f1dc0aec
1251303815@qq.com <1251303815@qq.com>
2019-02-26 18:19:47 +0800

    添加插入和查询功能

--- src/main/java/com/service/UserService.java
+++ src/main/java/com/service/UserService.java
@@ -2,6 +2,10 @@ package com.service;
 
 import com.model.pro.User;
 
+import java.util.List;
+
 public interface UserService {
-        User getUserById(int id);
+        User getUserById(int id)throws Exception;
+        void recruitment(List<User> users)throws Exception;
+        List<User> northernExpedition(User record)throws Exception;
 }
